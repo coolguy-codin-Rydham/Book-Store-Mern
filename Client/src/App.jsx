bb7119348a91fcb29620b13eaa6cbd8997427e19
coolguy-codin-Rydham <rydhampreetsingh.gindra@gmail.com>
2024-06-14 00:42:50 +0530

    Yo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react"
+import axios from "axios"
 function App() {
+  useEffect(()=>{
+    const getData = async()=>
+      {
+        const res = await axios.get("http://localhost:5555/books");
+        const data = res.data
+        console.log(data);
+      }
+      getData();
 
+  }, [])
   return (
     <div>
 
